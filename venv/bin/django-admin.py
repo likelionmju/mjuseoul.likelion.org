8743da9ec6b55e852d857651a27141683828a865
Jongwoo <han980817@gmail.com>
2019-02-16 01:06:20 +0900

    장고 기본 설정

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hanjongwoo/Desktop/개발자도구/Github/likelion_mju/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
